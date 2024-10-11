57813aaac5d4267d841197fb375cbe5fd8794e99
Eamon Boyle <eamon.boyle2@watchportfoliomanagement.com>
2024-10-11 16:48:44 +0100

    feat: add shadcn components and fix the lint errors in them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 export default function Home() {
-    return <div>Hello world</div>
+    return (
+        <div className="flex gap-4">
+            <Input />
+            <Button>Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="muted">Muted</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="teritary">Teritary</Button>
+        </div>
+    )
 }
